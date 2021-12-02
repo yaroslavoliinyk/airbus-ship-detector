8faeedc67142d9a8e4f2ae441829e54893400015
Yaroslav Oliinyk <y.oliyn7@gmail.com>
2021-12-02 22:00:19 +0200

    Feat: Assign locations to images that are downloaded.

---
+++ instruments/dsloader.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+import os
+import glob
+import re
+
+
+class DatasetLoader:
+    def __init__(self, preprocessors=None):
+        self.preprocessors = preprocessors
+        if self.preprocessors is None:
+            self.preprocessors = []
+
+    def load(self, imagePaths, verbose=-1):
+        data = []
+        labels = []
+
+        for (i, imagePath) in enumerate(imagePaths):
+            image = cv2.imread(imagePath)
+            label = imagePath.split(os.path.sep)[-1]
+
+            if self.preprocessors is not None:
+                for p in self.preprocessors:
+                    image = p.preprocess(image)
+            # treat our processed image as a "feature vector"
+            # by updating the data list followed by the labels
+            data.append(image)
+            labels.append(label)
+            # show an update every ‘verbose‘ images
+            if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1, len(imagePaths)))
+        # return a tuple of the data and labels
+        return (np.array(data), np.array(labels))
+
+    def save(
+        self,
+        trainX,
+        testX,
+        trainY,
+        testY,
+        trainPath,
+        testPath,
+    ):
+        # remove previous saved train and test images
+        train_file_list = glob.glob(os.path.join(trainPath, "*"))
+        for f in train_file_list:
+            os.remove(f)
+        test_file_list = glob.glob(os.path.join(testPath, "*"))
+        for f in test_file_list:
+            os.remove(f)
+        # overwrite train images where image name == trainY label
+        for (i, train_img) in enumerate(trainX):
+            full_path = trainPath + str(i) + "_" + trainY[i] + ".jpg"
+            cv2.imwrite(full_path, train_img)
+        # overwrite test images where image name == testY label
+        for (i, test_img) in enumerate(testX):
+            full_path = testPath + str(i) + "_" + testY[i] + ".jpg"
+            cv2.imwrite(full_path, test_img)
--- ship_detector.py
+++ ship_detector.py
@@ -1,1 +1,32 @@
 import os
+import pandas as pd
+from instruments.dsloader import DatasetLoader
+from instruments.preprocessor import SimplePreprocessor
+from imutils import paths
+from pathlib import Path
+
+# Path to file that has the script:
+project_dir = os.path.dirname(__file__)
+img_folder = os.path.join(project_dir, "data/train_v2/")
+imagePaths = sorted(list(paths.list_images("{}".format(img_folder))))
+preprocesssor = SimplePreprocessor(32, 32)
+dsl = DatasetLoader([preprocesssor])
+loc_path = os.path.join(project_dir, "data/train_ship_segmentations_v2.csv")
+
+# Get image locations
+def get_img_loc(loc_path, labels):
+    loc_data = pd.read_csv(loc_path)
+    loc_data.set_index("ImageId", inplace=True)
+    labels = labels.tolist()
+    labels_data = loc_data.loc[labels]
+    print(labels_data)
+
+
+# A method, that combines all information into a structured dict
+def get_info_dict(imagePaths, loc_path):
+    info_dict = dict()
+    images, labels = dsl.load(imagePaths)
+    loc_dict = get_img_loc(loc_path, labels)
+
+
+get_info_dict(imagePaths, loc_path)
